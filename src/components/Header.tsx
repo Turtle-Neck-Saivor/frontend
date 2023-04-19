526a179cff64d333e83b582765887ea908cc01d2
rabbit-22 <chlwlal20@gmail.com>
2023-04-19 20:21:28 +0900

    feat: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/Logo.png';
+
+/**
+ * 상단 헤더 컴포넌트
+ */
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <HeaderLayout>
+      <Image src={logo} />
+    </HeaderLayout>
+  );
 };
 
 export default Header;
+
+const HeaderLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100vw;
+  height: 4rem;
+  border-bottom: 1px solid #c8cbd9;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
+const Image = styled.img`
+  display: flex;
+  padding: 1rem;
+`;
