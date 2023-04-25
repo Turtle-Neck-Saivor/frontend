2993d8fb606c2c5b3e6a2091ae21c1003b0f5286
rabbit-22 <chlwlal20@gmail.com>
2023-04-25 21:31:34 +0900

    feat: add mediapipe holistic

--- src/components/camera/Camera.tsx
+++ src/components/camera/Camera.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Peer from 'simple-peer';
-import * as io from 'socket.io-client';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Guide from './Guide';
 import Webcam from 'react-webcam';
+import useHolistic from '../../hooks/useHolistic';
 
 /**
  *  camera 컴포넌트
  */
 
 const Camera = () => {
-  const socket = io.connect('');
-  const [stream, setStream] = useState<MediaStream>();
   const videoRef = useRef<Webcam>(null);
+  const { canvasRef } = useHolistic({
+    videoRef: videoRef,
+    isDetect: true,
+  });
 
   return (
     <VideoLayout>
       <Guide />
       <VideoBox>
+        <Canvas ref={canvasRef} />
         <Webcam
           audio={false}
           ref={videoRef}
@@ -41,6 +43,21 @@ const VideoBox = styled.div`
   width: 50vw;
   border-radius: 1.5rem;
   margin-left: 11rem;
+  @media all and (min-width: 768px) and (max-width: 1160px) {
+    width: 70vw;
+    margin: 0;
+  }
+  @media all and (max-width: 767px) {
+    width: 80vw;
+    margin: 0;
+  }
+`;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  border-radius: 1.5rem;
+  z-index: 9;
+  width: 50vw;
   @media all and (min-width: 768px) and (max-width: 1160px) {
     width: 70vw;
     margin: 0;
---
+++ src/hooks/useHolistic.ts
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef } from 'react';
+import { Holistic, POSE_CONNECTIONS } from '@mediapipe/holistic';
+import * as cam from '@mediapipe/camera_utils';
+import * as h from '@mediapipe/holistic';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+import Webcam from 'react-webcam';
+
+interface MediapipeDataProps {
+  poseLandmarks: h.NormalizedLandmarkList;
+}
+
+const useHolistic = ({
+  videoRef,
+  isDetect,
+}: {
+  videoRef: React.RefObject<Webcam>;
+  isDetect: boolean;
+}) => {
+  const holistic = new Holistic({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+    },
+  });
+  holistic.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: true,
+    smoothSegmentation: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5,
+    enableFaceGeometry: true,
+  });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const onResults: h.ResultsListener = (results) => {
+    if (!canvasRef.current || !videoRef.current?.video || !isDetect) {
+      return;
+    }
+
+    canvasRef.current.width = videoRef.current?.video.videoWidth;
+    canvasRef.current.height = videoRef.current?.video.videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext('2d');
+
+    if (!canvasCtx) {
+      return;
+    }
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.globalCompositeOperation = 'source-in';
+    canvasCtx.fillStyle = '#00FF00';
+    canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height,
+    );
+    canvasCtx.globalCompositeOperation = 'source-over';
+
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+      color: '#00FF00',
+      lineWidth: 3,
+    });
+    drawLandmarks(canvasCtx, results.poseLandmarks, {
+      color: '#FF0000',
+      lineWidth: 2,
+    });
+    drawConnectors(canvasCtx, results.faceLandmarks, h.FACEMESH_TESSELATION, {
+      color: '#C0C0C070',
+      lineWidth: 1,
+    });
+  };
+
+  useEffect(() => {
+    let camera: cam.Camera | null = null;
+    let isCanceled = false;
+
+    if (typeof videoRef.current !== 'undefined' && videoRef.current !== null) {
+      if (!videoRef.current?.video) {
+        return;
+      }
+
+      camera = new cam.Camera(videoRef.current?.video, {
+        onFrame: async () => {
+          if (!videoRef.current?.video || isCanceled) {
+            return;
+          }
+          await holistic.send({ image: videoRef.current?.video });
+        },
+      });
+      camera.start();
+    }
+    return () => {
+      isCanceled = true;
+      holistic.onResults(() => undefined);
+      camera?.stop();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isDetect) {
+      holistic.onResults(onResults);
+    } else {
+      holistic.onResults(() => undefined);
+    }
+  }, [isDetect]);
+
+  return {
+    canvasRef,
+  };
+};
+
+export default useHolistic;
