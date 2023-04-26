e8534665360b438daafb15192811e9bd3e3e0572
rabbit-22 <chlwlal20@gmail.com>
2023-04-27 04:14:02 +0900

    feat: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../assets/Logo.png';
+import logo from '../assets/turtle_logo.png';
 
 /**
  * 상단 헤더 컴포넌트
@@ -10,6 +10,7 @@ const Header = () => {
   return (
     <HeaderLayout>
       <Image src={logo} />
+      <Text>TurtleNeck</Text>
     </HeaderLayout>
   );
 };
@@ -18,7 +19,7 @@ export default Header;
 
 const HeaderLayout = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: start;
   width: 100vw;
   height: 4rem;
   border-bottom: 1px solid #c8cbd9;
@@ -27,5 +28,13 @@ const HeaderLayout = styled.div`
 const Image = styled.img`
   display: flex;
   padding: 1rem;
+  width: 2.1rem;
   margin-left: 1.5rem;
+`;
+
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  color: #302f4d;
+  font-weight: bold;
 `;
