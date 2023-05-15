275a198ff06b34fee4c63024323c84ea7e8251c5
rabbit-22 <chlwlal20@gmail.com>
2023-05-15 16:43:38 +0900

    feat: make Doughnut Chart

--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import DoughnutChart from '../components/report/DoughnutChart';
 
 const ReportPage = () => {
-  return <ReportPageLayout></ReportPageLayout>;
+  return (
+    <ReportPageLayout>
+      <DoughnutChart />
+    </ReportPageLayout>
+  );
 };
 
 export default ReportPage;
