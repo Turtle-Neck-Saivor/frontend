156b84c2db7497699cc6bd7b7d2574ac72be17d3
rabbit-22 <chlwlal20@gmail.com>
2023-06-10 01:51:20 +0900

    feat: Linear Regression Chart

---
+++ src/components/report/RegressionChart.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import Chart from 'chart.js';
+import styled from 'styled-components';
+import { Line, Scatter } from 'react-chartjs-2';
+import * as ss from 'simple-statistics';
+import { ChartLayout } from './ChartLayout.style';
+import ChartTitle from './ChartTitle';
+
+type DataPoint = { x: number; y: number };
+type DataSet = {
+  label: string;
+  data: DataPoint[];
+  fill: boolean;
+  type?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  pointBackgroundColor?: string;
+  pointBorderColor?: string;
+  showLine?: boolean;
+};
+type ChartData = { datasets: DataSet[] };
+
+const RegressionChart = ({ data }: { data: DataPoint[] }) => {
+  const [chartData, setChartData] = useState<ChartData>({
+    datasets: [
+      {
+        type: 'scatter',
+        label: 'Scatter Dataset',
+        data,
+        fill: false,
+        backgroundColor: '#000000',
+        borderColor: '#000000',
+      },
+      {
+        type: 'line',
+        label: 'Regression Line Dataset',
+        data: [],
+        fill: false,
+        borderColor: '#17a161',
+        showLine: true,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const regressionLine = ss.linearRegressionLine(
+      ss.linearRegression(data.map((d) => [d.x, d.y])),
+    );
+    const newChartData = data.map((point) => ({
+      x: point.x,
+      y: regressionLine(point.x),
+    }));
+
+    setChartData((prev) => ({
+      datasets: [
+        {
+          ...prev.datasets[0],
+          data: data,
+        },
+        {
+          ...prev.datasets[1],
+          data: newChartData,
+        },
+      ],
+    }));
+  }, [data]);
+
+  return (
+    <RegressionChartLayout>
+      <ChartTitle title="Month" />
+      <ChartContainer>
+        <Scatter
+          width={920}
+          height={300}
+          data={chartData}
+          options={{
+            responsive: false,
+            scales: {
+              x: { type: 'linear' },
+              y: { beginAtZero: true },
+            },
+          }}
+        />
+      </ChartContainer>
+    </RegressionChartLayout>
+  );
+};
+
+export default RegressionChart;
+
+const RegressionChartLayout = styled(ChartLayout)`
+  width: 85%;
+  height: 19rem;
+  padding-left: 1.5rem;
+`;
+
+const ChartContainer = styled.div`
+  margin: 1rem;
+`;
--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import DoughnutChart from '../components/report/DoughnutChart';
 import WeekBarChart from '../components/report/WeekBarChart';
-
+import RegressionChart from '../components/report/RegressionChart';
+import { Button } from '@mui/material';
 const ReportPage = () => {
+  const [data, setData] = useState([
+    { x: 0, y: 6 },
+    { x: 1, y: 3 },
+    { x: 2, y: 7 },
+    { x: 3, y: 2 },
+    { x: 4, y: 8 },
+    { x: 5, y: 3 },
+    { x: 6, y: 9 },
+    { x: 7, y: 2 },
+    { x: 8, y: 8 },
+    { x: 9, y: 3 },
+    { x: 10, y: 9 },
+  ]);
+  const addData = () => {
+    const x = data.length + 1;
+    const y = Math.floor(Math.random() * 10) + 1;
+    const newData = { x, y };
+    setData([...data, newData]);
+  };
   return (
     <ReportPageLayout>
       <ChartContainer>
         <DoughnutChart />
         <WeekBarChart />
       </ChartContainer>
+      <RegressionChart data={data} />
+      <Button onClick={addData}>데이터 추가</Button>
     </ReportPageLayout>
   );
 };
