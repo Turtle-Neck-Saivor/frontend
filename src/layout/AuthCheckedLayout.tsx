d76e0b5f909dbf969407c620392b6b264c6d8874
Jimi Choi <77673029+rabbit-22@users.noreply.github.com>
2023-09-08 23:55:42 +0900

    fix: Authchecked

--- src/layout/AuthCheckedLayout.tsx
+++ src/layout/AuthCheckedLayout.tsx
@@ -1,26 +1,23 @@
 import React, { Suspense, useEffect } from 'react';
-import { Provider } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import styled from 'styled-components';
 import Loading from '../components/Loading';
 import { useNavigate } from 'react-router-dom';
-import store from '../stores';
+import { RootState } from '../stores';
 
 /**
  *  사용자 인증이 필요한 페이지에게 보여지는 레이아웃
- *  TODO
- *  1. 렌더링 후 사용자 인증 검사 -> 안돼있으면 로그인으로 리다이렉트
  */
-
 const AuthCheckedLayout = ({ children }: { children: React.ReactNode }) => {
-  // const navigate = useNavigate();
-  // const nickname = useSelector((state: RootState) => state.user.nickname);
-  // useEffect(() => {
-  //   if (!localStorage.getItem('nickname')) {
-  //     navigate('/login');
-  //   }
-  // }, [children]);
+  const navigate = useNavigate();
+  const nickname = useSelector((state: RootState) => state.user.nickname);
+ useEffect(() => {
+    if (nickname === '') {
+      navigate('/login');
+    }
+  }, [children]);
 
   return (
     <RootLayout>
